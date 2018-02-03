970006b8cb5236b93386e5a9df21ffd01d59fefe
Juanbf <bulfonjf@gmail.com>
2018-02-03 01:51:41 -0300

    Initial commit

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,7 @@
+export default {
+  apiKey: 'AIzaSyAoH2oeQsHJXTDDbxmgeS05ChlcKvkFzJM',
+  authDomain: 'primosapp-193406.firebaseapp.com',
+  databaseURL: 'https://primosapp-193406.firebaseio.com',
+  projectId: 'primosapp-193406',
+  storageBucket: 'primosapp-193406.appspot.com',
+  messagingSenderId: '476628400718' };
